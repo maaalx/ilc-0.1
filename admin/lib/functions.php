bac4455f91a69f7f2329f74b2c50bd908116c876
jobykhoo <31298884+jobykhoo@users.noreply.github.com>
2017-10-11 02:03:51 +1100

    Add files via upload

--- admin/lib/functions.php
+++ admin/lib/functions.php
@@ -2,6 +2,15 @@
 function checkPermissions($user_id,$permissions){
 	global $db;
 	$flag=0;
+	
+	foreach($permissions as $val){
+		
+		if($val==4){
+		$query=mysqli_query($db,"select * from users where user_id='$user_id' and (user_type='2' OR user_type='3') and is_reseacher='yes'");
+		if(mysqli_num_rows($query)>0)return 1;
+		}
+		
+	}
 	foreach($permissions as $val){
 		
 		$query=mysqli_query($db,"select * from users where user_id='$user_id' and user_type='$val'");
@@ -21,6 +30,16 @@ function checkPatientBelongsToDoctor($patientId,$doctorId){
 	$total_rows=mysqli_fetch_assoc($query);
 	
 	if($total_rows>0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+function show_only_for_searcher($user_id){
+	global $db;
+	$query=mysqli_query($db,"select * from users where user_id='$user_id' and user_type='4'");
+	if(mysqli_num_rows($query)>0){
 		return 1;
 	}else{
 		return 0;
