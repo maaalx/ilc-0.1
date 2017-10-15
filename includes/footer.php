9628bf906e756961f4f8521f6c6cd10638fd0ac0
maaalx <maaalx@hotmail.com>
2017-10-16 06:14:41 +1100

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,18 @@
+<html>
+    
+<head>
+    
+    
+    <script type="text/javascript"> //<![CDATA[ 
+var tlJsHost = ((window.location.protocol == "https:") ? "https://secure.comodo.com/" : "http://www.trustlogo.com/");
+document.write(unescape("%3Cscript src='" + tlJsHost + "trustlogo/javascript/trustlogo.js' type='text/javascript'%3E%3C/script%3E"));
+//]]>
+</script>
+</head>    
+    
+<body>
+
+
 <style>
 #contactus{
 
@@ -81,6 +96,12 @@
 			</li>
 			<li>ILC@gmail.com</li>
 		</ul>
+		<br>
+</div>
+<script language="JavaScript" type="text/javascript">
+TrustLogo("http://maaalx.com/comodo_secure_seal.png", "CL1", "none");
+</script>
+<a  href="https://ssl.comodo.com" id="comodoTL">SSL Certificates</a>
 	</div>
 </div>
 
